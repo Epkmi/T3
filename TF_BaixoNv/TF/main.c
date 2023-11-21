0afc9ce73706ac1bd4a63d2899ae27d05f4cf929
Epkmi <116769179+Epkmi@users.noreply.github.com>
2023-11-21 14:03:36 -0300

    Update main.c

--- TF_BaixoNv/TF/main.c
+++ TF_BaixoNv/TF/main.c
@@ -84,12 +84,14 @@ void displayLifeGlyphs(int lives) {
 
 void clearLifeGlyph(int lives) {
     // Limpa o caractere de vida no canto superior direito
-    int startX = LCDWIDTH -4- (3*(lives));
-
+    int startX = LCDWIDTH -4- (4*(lives));
+int startY=0;
     for (int i = 0; i < 3; i++) {
         for (int j = 0; j < 3; j++) {
-         nokia_lcd_set_pixel(startX + j, i, 0);        }
-    }
+         nokia_lcd_set_pixel(startX + j, startY+i, 0);   
+  }
+    }        nokia_lcd_set_pixel(startX + 4, startY+4, 0);   
+
 }
 
 void clearAnotherGlyph(int x, int y) {
@@ -132,37 +134,41 @@ void moveObject(Object *obj, int targetX, int targetY, int *lives) {
         // Limpa o caractere de vida correspondente
         clearLifeGlyph(*lives);
     }
-        _delay_ms(30);
-
+    _delay_ms(30);
 }
 
-void generateRandomObject(Object *obj, int *objectsInCenter) {
+void generateRandomObject(Object *objects, int *objectsInCenter) {
     // Gera um objeto apenas se a quantidade de objetos no centro for menor que 4
     if (*objectsInCenter < 4) {
         int side = rand() % 4; // 0: top, 1: bottom, 2: left, 3: right
 
         switch (side) {
             case 0:
-                obj->x = rand() % (LCDWIDTH - OBJECT_WIDTH);
-                obj->y = 0;
+                // Topo
+                objects[*objectsInCenter].x = rand() % (LCDWIDTH - OBJECT_WIDTH);
+                objects[*objectsInCenter].y = 0;
                 break;
             case 1:
-                obj->x = rand() % (LCDWIDTH - OBJECT_WIDTH);
-                obj->y = LCDHEIGHT - OBJECT_HEIGHT;
+                // Base
+                objects[*objectsInCenter].x = rand() % (LCDWIDTH - OBJECT_WIDTH);
+                objects[*objectsInCenter].y = LCDHEIGHT - OBJECT_HEIGHT;
                 break;
             case 2:
-                obj->x = 0;
-                obj->y = rand() % (LCDHEIGHT - OBJECT_HEIGHT);
+                // Esquerda
+                objects[*objectsInCenter].x = 0;
+                objects[*objectsInCenter].y = rand() % (LCDHEIGHT - OBJECT_HEIGHT);
                 break;
             case 3:
-                obj->x = LCDWIDTH - OBJECT_WIDTH;
-                obj->y = rand() % (LCDHEIGHT - OBJECT_HEIGHT);
+                // Direita
+                objects[*objectsInCenter].x = LCDWIDTH - OBJECT_WIDTH;
+                objects[*objectsInCenter].y = rand() % (LCDHEIGHT - OBJECT_HEIGHT);
                 break;
         }
 
         (*objectsInCenter)++;
     }
 }
+
 
 int main(void) {
     nokia_lcd_init();
@@ -171,6 +177,7 @@ int main(void) {
     srand(time(NULL)); // Inicializa a semente para números aleatórios
 
     Object movingObject;
+    Object objects[4];  // Array para rastrear objetos
     int targetX, targetY;
     int objectsInCenter = 0;
     int lives = 3;  // Inicializa o contador de vidas
@@ -178,16 +185,17 @@ int main(void) {
     while (1) {
         // Gere um objeto a cada 5 segundos em uma direção aleatória
         if (rand() % 100 < 20) { // Ajuste o valor para controlar a frequência de geração
-            generateRandomObject(&movingObject, &objectsInCenter);
+            generateRandomObject(objects, &objectsInCenter);
             targetX = (LCDWIDTH - OBJECT_WIDTH) / 2;
             targetY = (LCDHEIGHT - OBJECT_HEIGHT) / 2;
         }
 
-        // Mova o objeto em direção ao centro
-        moveObject(&movingObject, targetX, targetY, &lives);
-
-        // Exiba o objeto em sua nova posição
-        displayAnotherGlyph(movingObject.x, movingObject.y);
+        // Mova todos os objetos em direção ao centro
+        for (int i = 0; i < objectsInCenter; i++) {
+            moveObject(&objects[i], targetX, targetY, &lives);
+            // Exiba cada objeto em sua nova posição
+            displayAnotherGlyph(objects[i].x, objects[i].y);
+        }
 
         // Exiba o outro Glyph em uma posição fixa (por exemplo, canto superior direito)
         displayAnotherGlyph(0, 0);
@@ -201,8 +209,8 @@ int main(void) {
         // Atualize o display
         nokia_lcd_render();
 
-        if(lives==0){
-            nokia_lcd_write_string("ACABOU BROTHER DESISTE",1);
+        if (lives == 0) {
+            nokia_lcd_write_string("ACABOU BROTHER DESISTE", 1);
             return 1;
         }
 
